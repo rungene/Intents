02fdc2ef4d39b7c9956fca6cfc2b6b5ebf3f345c
rungene <rungenesamuel@gmail.com>
2018-04-28 23:27:31 +0300

    cnaging images in the drawabels using image resource

--- app/src/main/java/com/rungene/intents/MainActivity.java
+++ app/src/main/java/com/rungene/intents/MainActivity.java
@@ -1,10 +1,10 @@
 package com.rungene.intents;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +23,12 @@ public class MainActivity extends AppCompatActivity {
     }
     public void   openSecondCat(View view){
 
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        image.setImageResource(R.drawable.cat2);
+
         //Toast.makeText(this, "Hi cat Two", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(MainActivity.this,ActivityTwo.class);
-        startActivity(intent);
+       /* Intent intent = new Intent(MainActivity.this,ActivityTwo.class);
+        startActivity(intent);*/
 
     }
 }
